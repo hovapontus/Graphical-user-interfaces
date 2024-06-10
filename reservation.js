4073af212729c6f36e8d85f511595e27d16f526e
hovapontus <106970440+hovapontus@users.noreply.github.com>
2024-06-10 12:08:03 +0200

    nytt

--- reservation.js
+++ reservation.js
@@ -8,22 +8,23 @@ $(document).ready(function () {
 
     //när användaren trycker på modal send ska namnet sparas detta ska ske på modal-alla-bord upptagna
     $("#addToQue").on("click", () => {
-        let firstName = $("#queNamea").val();
-        console.log(firstName);
-        addToReservation(GlobalObject, firstName);
+        let name = $("#queName").val();
+        let antalGaster = $('#reservationAntalGaster').val();
+        if (name == '') {
+            alert('Vänligen fyll i Namn');
+        } else {
+            addToReservation(GlobalObject, name, antalGaster);
+            $("#queName").val("");
+        }
 
-        $("#recipient-name").val("")
-
-    })
-
+    });
 });
 
-
-
-function addToReservation(GlobalObject, firstName) {
+function addToReservation(GlobalObject, firstName, antalGaster) {
     var TR = $("<tr></tr>"); // Skapa ett nytt tr-element
     var TH = $("<th></th>").attr("scope", "row").attr("id", "queId").text(GlobalObject.queId);
     var TDfirst = $("<td></td>").text(firstName);
+    var TDsecond = $('<td></td>').text(antalGaster);
     var closeKnapp = $("<button></button>").attr("class", "btn-close");
     //tar bort hela tr elementet när man trycker på knappen
     closeKnapp.on("click", function () {
@@ -31,12 +32,9 @@ function addToReservation(GlobalObject, firstName) {
     });
 
 
-
-    /**
-     * <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-     */
     TR.append(TH);
     TR.append(TDfirst);
+    TR.append(TDsecond);
     TR.append(closeKnapp);
 
     // Lägg till tr-elementet i tbody
